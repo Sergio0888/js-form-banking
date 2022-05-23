d6c861fc19dd206bbd57d61b38586fec78203f97
Sergio0888 <99532626+Sergio0888@users.noreply.github.com>
2022-05-23 19:36:38 +0300

    Добавил скелет

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const inputPriceEl = document.querySelector('.box-input')
+let balanceEl = document.querySelector('.balance')
+
+const topBtnEl = document.querySelector('.js-btn-top')
+const downBtnEl = document.querySelector('.js-btn-down')
+
+const historyEl = document.querySelector('.main-box__history')
+
+const account = {
+    balance: 0,
+    transactions: [],
+    id: [],
+    transAmount: [],
+  
+
+
+    deposit(amount) {
+      this.balance += amount;
+      this.transactions.push('Пополнение')
+      this.transAmount.push(amount)
+      this.id.push('_' + Math.random().toString(36).substr(2, 9))
+    },
+
+    withdraw(amount) {
+      if (amount > this.balance) {
+        alert('Снятие суммы невозможно, недостаточно средств')
+      } else {
+        this.balance -= amount;
+        this.transactions.push('Вывод')
+        this.transAmount.push(amount)
+        this.id.push('_' + Math.random().toString(36).substr(2, 9))
+      }
+    },
+
+  };
+
+let total = 0;
+
+const onTopBalance = (event) => {
+    account.deposit(Number(inputPriceEl.value))
+    balanceEl.textContent = account.balance
+
+    let newTransaction = document.createElement('a')
+    newTransaction.classList.add('main-box__number-hystory')
+    newTransaction.textContent = `${account.transactions[total]} на ${account.transAmount[total]} грн id:${account.id[total]}`
+    total += 1
+    historyEl.appendChild(newTransaction)
+}
+ topBtnEl.addEventListener('click', onTopBalance)
+
+
+
+const onDownBalance = (event) => {
+    account.withdraw(Number(inputPriceEl.value))
+    balanceEl.textContent = account.balance
+
+
+if (account.balance >= Number(inputPriceEl.value)) {
+    let newTransaction = document.createElement('a')
+    newTransaction.classList.add('main-box__number-hystory')
+    newTransaction.textContent = `${account.transactions[total]} на ${account.transAmount[total]} грн id:${account.id[total]}`
+    total += 1
+    historyEl.appendChild(newTransaction)
+}
+
+
+}
+downBtnEl.addEventListener('click', onDownBalance)
+
+ console.log(account.balance);
+
