c43b20dacc49c488fd911d7a15942d56620eaab1
Sergei Maznoi <s.maznoi888@gmail.com>
2022-05-27 13:56:47 +0300

    login form confirm

--- js/login-check.js
+++ js/login-check.js
@@ -7,12 +7,11 @@ const openBtnEl = document.querySelector('.js-btn')
 const errorBtnEl = document.querySelector('.error-text')
 
 
-
-
+// Проверка Логина и Пароля
 
 const checkLogin = (event) => {
     const login = loginFormEl.value.toLowerCase()
-    const password = passwordFormEl.value.toLowerCase()
+    let password = passwordFormEl.value.toLowerCase()
 
 
     if(login === 'admin' && password === 'goit') {
@@ -21,14 +20,66 @@ const checkLogin = (event) => {
     }
     errorBtnEl.classList.add('is-open')
     openBtnEl.href = '#'
+
+    onResetForm(formEl)
     return
     
 }
+
+// Сброс формы
+
+function onResetForm(form) {
+    
+    formEl.reset()
+    localStorage.removeItem("login-form")
+}
+
+
+// Нажатие - Enter
 
 document.addEventListener('keydown', event => {
     if (event.code === 'Enter') {
         checkLogin(openBtnEl)
         openBtnEl.click()
+        onResetForm(formEl)
     }});
 
 openBtnEl.addEventListener('click', checkLogin)
+
+
+// Запоминание логина
+
+
+const formData = {}
+
+const onChangeForm = event => {
+    const {target} = event
+
+    const value = target.value
+    const name = target.name
+
+    formData[name] = value
+
+    localStorage.setItem("login-form", JSON.stringify(formData))
+}
+
+const onCheckedForm = form => {
+    const formDataLocal = JSON.parse(localStorage.getItem("login-form"))
+    const formElements = form.elements
+
+    for (const key in formDataLocal) {
+        if(formDataLocal.hasOwnProperty(key)) {
+            formElements[key].value = formDataLocal[key]
+        }
+    }
+}
+
+onCheckedForm(formEl)
+
+formEl.addEventListener('input', onChangeForm)
+
+
+
+
+
+
